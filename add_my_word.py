4e7e1ef0a14a913d8374b29d4ac01393fd2e8947
Kstyle0710 <jongbaekim0710@gmail.com>
2020-11-18 15:56:43 +0900

    add my words

---
+++ add_my_word.py
@@ -0,0 +1,43 @@
+# with open("C:/mecab/user-dic/nnp.csv", 'r', encoding='utf-8') as f:
+#     file_data = f.readlines()
+# print(file_data)
+
+# pip install jamo  ## 종성 여부를 판단하기 위한 라이브러리
+
+from jamo import h2j, j2hcj
+
+def get_jongsung_TF(sample_text):
+    sample_text_list = list(sample_text)
+    last_word = sample_text_list[-1]
+    last_word_jamo_list = list(j2hcj(h2j(last_word)))
+    last_jamo = last_word_jamo_list[-1]
+    jongsung_TF = "T"
+    if last_jamo in ['ㅏ', 'ㅑ', 'ㅓ', 'ㅕ', 'ㅗ', 'ㅛ', 'ㅜ', 'ㅠ', 'ㅡ', 'ㅣ', 'ㅘ', 'ㅚ', 'ㅙ', 'ㅝ', 'ㅞ', 'ㅢ', 'ㅐ,ㅔ', 'ㅟ', 'ㅖ', 'ㅒ']:
+        jongsung_TF = "F"
+    return jongsung_TF
+
+
+with open("C:/mecab/user-dic/nnp.csv", 'r', encoding='utf-8') as f:
+    file_data = f.readlines()
+    word_list = ['가우징']   ## 추가할 단어 리스트
+    for word in word_list:
+        jongsung_TF = get_jongsung_TF(word)
+        line = '{},,,,NNP,*,{},{},*,*,*,*,*\n'.format(word, jongsung_TF, word)
+        file_data.append(line)
+
+
+##
+with open("C:/mecab/user-dic/nnp.csv", 'w', encoding='utf-8') as f:
+    for line in file_data:
+        f.write(line)
+
+###
+with open("C:/mecab/user-dic/nnp.csv", 'r', encoding='utf-8') as f:
+    file_new = f.readlines()
+
+# print(file_new)
+
+
+
+
+
