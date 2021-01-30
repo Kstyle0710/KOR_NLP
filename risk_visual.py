b063d9326de5387d5e31e9f94d57438a40c3f172
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-01-30 16:11:23 +0900

    modifying dept, 대구분

--- risk_visual.py
+++ risk_visual.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import plotly.express as px
-import dash         #(version 1.0.0)
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Output, Input, State
+import dash_extensions as de    # pip install dash-extensions
 
 # ## df 세팅
 df_visual = pd.read_excel('./result/mother_file10.xlsx').fillna("")   ## ws_risk_mother 실행 결과인 엑셀 읽어오기
@@ -36,15 +37,27 @@ app.layout = html.Div([
 
         html.Div([
             html.Label(['부서'],style={'font-weight': 'bold'}),
-            dcc.RadioItems(
-                id='xaxis_raditem',
+            dcc.Dropdown(
+                id='dept-dropdown',
                 options=[
-                         {'label': '부서', 'value': '부서'},
-                         {'label': '대구분', 'value': '대구분'},
-                ],
-                value='부서',
-                style={"width": "50%"}
-            ),
+                    {'label': '가공소조립부', 'value': '가공소조립부'},
+                    {'label': '판넬조립부', 'value': '판넬조립부'},
+                    {'label': '대조립1부', 'value': '대조립1부'},
+                    {'label': '대조립2부', 'value': '대조립2부'},
+                    {'label': '선행의장부', 'value': '선행의장부'},
+                    {'label': '의장생산부', 'value': '의장생산부'},
+                    {'label': 'Unit생산부', 'value': 'Unit생산부'},
+                    {'label': '내업공사지원부', 'value': '내업공사지원부'},
+                    {'label': '선행도장부', 'value': '선행도장부'},
+                    {'label': '선실생산부', 'value': '선실생산부'},
+                    {'label': '건조1부', 'value': '건조1부'},
+                    {'label': '건조2부', 'value': '건조2부'},
+                    {'label': '건조3부', 'value': '건조3부'},
+                    {'label': '의장1부', 'value': '의장1부'},
+                    ],
+                value='가공소조립부',
+                multi=False,
+                ),
         ]),
 
         html.Div([
@@ -59,6 +72,8 @@ app.layout = html.Div([
                 value='R_vector',
                 style={"width": "50%"}
             ),
+        html.Br(),
+        html.Button(id='submit-button', n_clicks=0, children="Submit")
         ]),
 
     html.Div([
@@ -70,31 +85,27 @@ app.layout = html.Div([
 
 @app.callback(
     Output(component_id='the_graph', component_property='figure'),
-    [Input(component_id='xaxis_raditem', component_property='value'),
-     Input(component_id='yaxis_raditem', component_property='value')]
-)
-def update_graph(x_axis, y_axis):
+    [Input(component_id="submit-button", component_property="n_clicks")],
+    [State(component_id='dept-dropdown', component_property='value'),
+     State(component_id='yaxis_raditem', component_property='value')],prevent_initial_call=False)
 
-    dff = gdf
+def update_graph(n_clicks, dept, y_axis):
+
+    dff = gdf[gdf["부서"] == dept]
     barchart=px.bar(
             data_frame=dff,
-            x= x_axis,
+            x= dff['대구분'],
             y= y_axis,
             title="Risk in Working Standard",
-            # facet_col='Borough',
-            # color='대공정',
-            # barmode='group',
+            color='중구분',
+            hover_data=["대구분", "중구분", "소구분", "WORK_STAND_NM"],
             )
 
     barchart.update_layout(xaxis={'categoryorder':'total ascending'},
                            title={'xanchor':'center', 'yanchor': 'top', 'y':0.9,'x':0.5,},
+                           height=700,
                            )
-
     return (barchart)
 
-
-
-
 if __name__ == '__main__':
-    # app.run_server(port = 4040)
     app.run_server(debug=True)
